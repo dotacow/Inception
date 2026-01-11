aa54cd98c6bc015e436e77655fb5ebfb39d84283
dotacow <ykitaneh22@gmail.com>
2026-01-11 21:04:19 +0300

    add: pipefail option safety

--- srcs/tools/get_penstable.sh
+++ srcs/tools/get_penstable.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ENV_FILE="${SCRIPT_DIR}/../.env"
 
+set -euo pipefail
+
 VERSION=$(curl -s "https://hub.docker.com/v2/repositories/library/alpine/tags?page_size=100" | \
   jq -r '.results[].name' | \
   grep -E '^[0-9]+\.[0-9]+$' | \
